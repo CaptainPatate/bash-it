8c17a14e5899bdecf329d9d0ca00fca180cd802c
Amaury Gauthier <amaury@lumapps.com>
2018-06-29 13:57:02 +0200

    Update alias

--- aliases/custom.aliases.bash
+++ aliases/custom.aliases.bash
@@ -1,4 +1,4 @@
 alias joe='jmacs -nobackups'
 alias startx='ssh-agent startx'
-alias s2ram='sudo /usr/sbin/s2ram'
-alias sshadd='ssh-add -t 18000 ~/.ssh/google_compute_engine ~/.ssh/id_rsa'
+alias sshadd='ssh-add -t 18000 ~/.ssh/google_compute_engine ~/.ssh/id_ecdsa'
+alias bashnorc='bash --noprofile --norc'
